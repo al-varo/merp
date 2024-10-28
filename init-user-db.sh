60346fb7bae85c797ea83b0ee98f8cb8656688bc
al-varo <52884208+al-varo@users.noreply.github.com>
2024-10-29 04:20:33 +0700

    Update init-user-db.sh

--- init-user-db.sh
+++ init-user-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-        CREATE USER lambertus WITH SUPERUSER PASSWORD '@Hasbunalloh2024';
-        CREATE USER demo WITH SUPERUSER PASSWORD '@Hasbunalloh2024';
-        CREATE USER odoo WITH SUPERUSER PASSWORD '@Hasbunalloh2024';
-        CREATE USER offline WITH PASSWORD 'ra#asia';
+        CREATE USER lambertus WITH SUPERUSER PASSWORD '@changeme@';
+        CREATE USER demo WITH SUPERUSER PASSWORD '@changeme@';
+        CREATE USER odoo WITH SUPERUSER PASSWORD '@changeme@';
+        CREATE USER offline WITH PASSWORD '@changeme@';
 EOSQL
